57ceac08077512ba6082bdc0519be38e6c39c5ac
Wilker <Wilker@PC>
2014-10-16 21:27:19 -0300

    Alterado Header para refletir o arquivo .c

---
+++ Freecell.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Freecell.h
+ * Author: Wilker
+ *
+ * Created on 16 de Outubro de 2014, 20:49
+ */
+
+#ifndef FREECELL_H
+#define	FREECELL_H
+
+void criaMesa (TPilha **cartas, TPilha **naipe, TPilha **reserva);
+void preenchePilhas(TPilha *pilha, char* caminho);
+
+#endif	/* FREECELL_H */
