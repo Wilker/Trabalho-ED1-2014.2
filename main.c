8ae135feceaed702d50045051213be924a57a53e
Wilker <Wilker@PC>
2014-10-16 21:48:40 -0300

    Resolvido problema de ler a ultima linha duas vezes!

--- main.c
+++ main.c
@@ -8,21 +8,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "TPilha.h"
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-     FILE* fp = fopen("C:\\Users\\Wilker\\Documents\\NetBeansProjects\\Trabalho-ED1-2014.2\\entrada.txt","r");
-    if(!fp){
+    FILE* fp = fopen("C:\\Users\\Wilker\\Documents\\NetBeansProjects\\Trabalho-ED1-2014.2\\entrada.txt", "r");
+    if (!fp) {
         printf("Erro na abertura do arquivo, tente novamente:");
         exit(1);
     }
-     while (!feof(fp)){
-         char tmp[10] ;
-         fscanf(fp," %2[^\n]",tmp);
-         printf("=> %s \n", tmp); // Por algum motivo tá imprimindo a ultima linha duas vezes, revisar!
+    char tmp[10];
+    fscanf(fp, " %2[^\n]", tmp); // lê até dois caracteres ou até um caractere de nova linha.
+    while (!feof(fp)) {
+        printf("=> %s \n", tmp); 
+        fscanf(fp, " %2[^\n]", tmp); // lê até dois caracteres ou até um caractere de nova linha.
     }
-     system("pause");
+    system("pause");
     return (EXIT_SUCCESS);
 }
 
