7f70affb45af77121ef611c5489a6815b0156f59
Wilker <Wilker@PC>
2014-10-16 21:22:41 -0300

    Teste para leitura de arquivo. Rodou porém está lendo a ultima linha duas vezes.

--- main.c
+++ main.c
@@ -12,7 +12,17 @@
  * 
  */
 int main(int argc, char** argv) {
-
+     FILE* fp = fopen("C:\\Users\\Wilker\\Documents\\NetBeansProjects\\Trabalho-ED1-2014.2\\entrada.txt","r");
+    if(!fp){
+        printf("Erro na abertura do arquivo, tente novamente:");
+        exit(1);
+    }
+     while (!feof(fp)){
+         char tmp[10] ;
+         fscanf(fp," %2[^\n]",tmp);
+         printf("=> %s \n", tmp); // Por algum motivo tá imprimindo a ultima linha duas vezes, revisar!
+    }
+     system("pause");
     return (EXIT_SUCCESS);
 }
 
