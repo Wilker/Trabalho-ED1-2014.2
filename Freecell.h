3fb879a4fbe39990d1ba337452f102d94a06a6b6
Wilker <Wilker@PC>
2014-11-05 21:23:34 -0200

    Alterado header para refletir arquivo .C

--- Freecell.h
+++ Freecell.h
@@ -8,14 +8,12 @@
 #ifndef FREECELL_H
 #define	FREECELL_H
 typedef struct freecell {
-    // 
-    // Mover para o arquivo.h
-    TPilha **cartas; // Pilhas de A a H
     TPilha **naipe; // Pilhas de 0 a 3
+    TPilha **cartas; // Pilhas de A a H
     TPilha **reserva; // Pilhas de W a Z
-} Freecell;
+}Freecell;
 
-void inicializaFreecell(Freecell *freecell);
+Freecell* inicializaFreecell();
 void criaMesa (Freecell *freecell);
 void preenchePilhaDeCartas(Freecell *freecell, char* caminho);
 void imprimePilhas(Freecell *freecell);
