b9df53bb5390626bfffb9bb369d329bc5a6c0d89
Wilker <Wilker@PC>
2014-11-15 23:00:53 -0200

    Removido linhas de testes, deixando apenas as necessárias à execução do programa!

--- main.c
+++ main.c
@@ -9,30 +9,17 @@
 /* 
  * 
  */
-int main(int argc, char **argv)
-{
-	printf("%d\n", argc);
-	printf("%s\n", argv[1]);
-	if (argc < 2)
-	{
-		printf("Passe o arquivo via linha de comando!!");
-		system("PAUSE");
-		exit(1);
-	}
-
-	// Cria um ponteiro para uma strutct do jogo alocada dinamicamente
-
-	printf("Antes do jogo");
-	Freecell *jogo = (Freecell *) malloc(sizeof(Freecell));
-   inicializaFreecell(jogo);
-    // jogo->cartas[0]criaPilhaDeCartas
-
-	// Cria a mesa do jogo;
-	criaMesa(jogo);
-	// Distribui a pilha de Cartas inicial com o caminho do arquivo passado
-	// via linha de comando 
-	//preenchePilhaDeCartas(jogo, argv[1]);
-	//free(jogo);
-	system("PAUSE");
-	return 0;
+int main(int argc, char **argv) {
+    printf("%d\n", argc);
+    printf("%s\n", argv[1]);
+    if (argc < 2) {
+        printf("Passe o arquivo via linha de comando!!");
+        system("PAUSE");
+        exit(1);
+    }
+    Freecell* jogo = inicializaFreecell();
+    preenchePilhaDeCartas(jogo, argv[1]);
+    free(jogo);
+    system("PAUSE");
+    return 0;
 }
